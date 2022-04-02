4c042be5aecdfc65a9e93ab1c671e612106924f7
srH-hype <jejeje300@gmail.com>
2022-04-01 22:18:32 -0700

    main y login

---
+++ app/src/main/java/imperial/heriberto/cuidavet/LoginActivity.kt
@@ -0,0 +1,11 @@
+package imperial.heriberto.cuidavet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
