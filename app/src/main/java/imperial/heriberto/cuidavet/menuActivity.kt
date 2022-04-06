e603e00f66dfd5491bcc700d827f90b69570eecb
srH-hype <jejeje300@gmail.com>
2022-04-05 20:33:02 -0600

    emergencia

--- app/src/main/java/imperial/heriberto/cuidavet/menuActivity.kt
+++ app/src/main/java/imperial/heriberto/cuidavet/menuActivity.kt
@@ -1,11 +1,19 @@
 package imperial.heriberto.cuidavet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class menuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val button: Button = findViewById(R.id.btn_emergencias)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, EmergenciaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
