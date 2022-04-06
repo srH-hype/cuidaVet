0f0aaf2e457c8b625bb72bf30ee1e09f3c0e393a
Euan32 <lorenzo.euan204239@potros.itson.edu.mx>
2022-04-05 19:57:12 -0700

    registro

---
+++ app/src/main/java/imperial/heriberto/cuidavet/Registro.kt
@@ -0,0 +1,19 @@
+package imperial.heriberto.cuidavet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        val button: Button = findViewById(R.id.btn_flcheaRegresar)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
