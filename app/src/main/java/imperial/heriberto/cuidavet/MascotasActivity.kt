cc669eca867d7daee9843375478518a20e2ed312
srH-hype <jejeje300@gmail.com>
2022-04-06 01:23:31 -0600

    mis mascotas

--- app/src/main/java/imperial/heriberto/cuidavet/MascotasActivity.kt
+++ app/src/main/java/imperial/heriberto/cuidavet/MascotasActivity.kt
@@ -1,11 +1,40 @@
 package imperial.heriberto.cuidavet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MascotasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mascotas)
+
+        val button: Button = findViewById(R.id.btn_regresar)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, menuActivity::class.java)
+            startActivity(intent)
+        }
+
+        val button1: Button = findViewById(R.id.btn_mascota1)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, mascotanuevaActivity::class.java)
+            startActivity(intent)
+        }
+        val button2: Button = findViewById(R.id.btn_mascota2)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, mascotanuevaActivity::class.java)
+            startActivity(intent)
+        }
+        val button3: Button = findViewById(R.id.btn_mascota3)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, mascotanuevaActivity::class.java)
+            startActivity(intent)
+        }
+        val button4: Button = findViewById(R.id.btn_mascota4)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, mascotanuevaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
