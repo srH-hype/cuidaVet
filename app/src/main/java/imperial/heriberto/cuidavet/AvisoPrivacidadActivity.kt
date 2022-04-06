b8705693d29de2a0bdbc0fc518ea91455df7193e
Euan32 <lorenzo.euan204239@potros.itson.edu.mx>
2022-04-05 20:47:27 -0700

    Aviso privacidad

---
+++ app/src/main/java/imperial/heriberto/cuidavet/AvisoPrivacidadActivity.kt
@@ -0,0 +1,20 @@
+package imperial.heriberto.cuidavet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class AvisoPrivacidadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aviso_privacidad)
+
+        val button: Button = findViewById(R.id.btn_hecho)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, Registro::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
