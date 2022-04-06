ae97763e3d513060f5283f60b0753c46f63bc1d7
srH-hype <jejeje300@gmail.com>
2022-04-06 01:42:12 -0600

    nueva mascota

--- app/src/main/java/imperial/heriberto/cuidavet/mascotanuevaActivity.kt
+++ app/src/main/java/imperial/heriberto/cuidavet/mascotanuevaActivity.kt
@@ -1,11 +1,20 @@
 package imperial.heriberto.cuidavet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class mascotanuevaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mascotanueva)
+
+
+        val button: Button = findViewById(R.id.btn_regresarMascota)
+        button.setOnClickListener{
+            var intent: Intent = Intent (this, MascotasActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
