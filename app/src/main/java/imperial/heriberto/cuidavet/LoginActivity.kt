57429f85baad405d61397cac3b2a61ca4bc76c2d
DaSwow <carls09995@hotmail.com>
2022-05-10 21:21:26 -0700

    Iniciar sesion con usuario pre hecho

--- app/src/main/java/imperial/heriberto/cuidavet/LoginActivity.kt
+++ app/src/main/java/imperial/heriberto/cuidavet/LoginActivity.kt
@@ -3,18 +3,43 @@ package imperial.heriberto.cuidavet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-
+import android.widget.Toast
+import imperial.heriberto.cuidavet.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val button: Button = findViewById(R.id.btn_iniciar)
-        button.setOnClickListener{
-            var intent: Intent = Intent (this, menuActivity::class.java)
-            startActivity(intent)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnIniciar.setOnClickListener{
+            val mEmail = binding.edUsuario.text.toString()
+            val mPassword=binding.edContra.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty()->{
+                    Toast.makeText(baseContext, "Mail o contraseña incorrecta.",
+                        Toast.LENGTH_SHORT).show()
+                }else->{
+                SignIn(mEmail,mPassword)
+            }
+
+            }
+
         }
+
+
 
         val button2: Button = findViewById(R.id.btn_usuarioNuevo)
         button2.setOnClickListener{
@@ -23,4 +48,25 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun SignIn(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    var intent: Intent = Intent (this, menuActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+    }
+
+
 }
